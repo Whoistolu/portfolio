67630ed4119c79f38b447d54833ba1f9f8fd929d
Whoistolu <digittolu25@gmail.com>
2024-11-03 14:31:38 +0100

    Update the Navbar component with the React social icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
+
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className="flex flex-shrink-0 items-center">
+            <img src={logo} alt="logo" />
+        </div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin/>
+            <FaGithub/>
+            <FaInstagram/>
+            <FaXTwitter/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
