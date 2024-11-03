0ce2ed2615cd22e858baf0d141ab10b7a86d35e9
Whoistolu <digittolu25@gmail.com>
2024-11-03 14:29:59 +0100

    Rename App.js to App.jsx and add some stayles and containers

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='overflow-x-hidden text-white antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className="h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+    <div className="container mx-auto px-8">
+      <Navbar />
+      <h1 className='text-white bg-blue-600'>test this</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
