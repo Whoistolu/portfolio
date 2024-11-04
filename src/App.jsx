a0d7b6691014f459aaa01f54baeb5226886545c1
Whoistolu <digittolu25@gmail.com>
2024-11-04 23:10:06 +0100

    Add more implementations

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './components/About'
 const App = () => {
   return (
     <div className='overflow-x-hidden text-white antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className="h-full w-full">
+      <div className="h-full w-full fixed top-0 -z-10">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
     <div className="container mx-auto px-8">
--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -4,7 +4,7 @@ import profilepicture from '../assets/profilep.png'
 
 const Legend = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+    <div className='border-b border-neutral-500 pb-4 lg:mb-35'>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
