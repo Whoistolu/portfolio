86dc8c1efd3e9e33dc387d8cb1a1c9360e6cf1a5
Whoistolu <digittolu25@gmail.com>
2024-11-04 20:04:59 +0100

    Add and styled profile picture to the Legend component

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { LEGEND_CONTENT } from '../constants';
+import profilepicture from '../assets/profilep.png'
 
 const Legend = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
-        <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start">
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Toluwase Ajise</h1>
-                    <span className='bg-gradient-to-r from-orange-400 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-4xl text-transparent'>Full Stack Dveloper</span>
-                </div>
-            </div>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Toluwase Ajise</h1>
+            <span className='bg-gradient-to-r from-orange-400 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-4xl text-transparent'>
+              Full Stack Dveloper
+            </span>
+            <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{LEGEND_CONTENT}</p>
+          </div>
         </div>
+        <div className='lg:w-1/2 lg:p-8'>
+          <div className="flex justify-center">
+            <img src={profilepicture} alt='profile-p' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `A proficient Full-Stack Developer with over 3+ years of experience in creating robust and intuitive web applications, covering both front-end interfaces and back-end functionality. I’m skilled in a wide range of technologies, including Ruby on Rails, React, TypeScript, HTML/CSS, PostgreSQL, and MySQL, with a focus on delivering seamless user experiences and reliable, scalable backend systems.`;
+export const LEGEND_CONTENT = `A proficient Full-Stack Developer with over 3+ years of experience in creating robust and intuitive web applications, covering both front-end interfaces and back-end functionality. I’m skilled in a wide range of technologies, including Ruby on Rails, React, TypeScript, HTML/CSS, PostgreSQL, and MySQL, with a focus on delivering seamless user experiences and reliable, scalable backend systems.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 3 years of professional experience, I have worked with a variety of technologies, including Ruby on Rails, React, MySQL, and PostgreSQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
