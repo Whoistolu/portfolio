6dc02d509a9cb3faed1ab68306a9d6ca198599b0
Whoistolu <digittolu25@gmail.com>
2024-11-04 15:56:34 +0100

    Re-style Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" />
+            <img className='mx-2 w-12' src={logo} alt="logo" />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin/>
