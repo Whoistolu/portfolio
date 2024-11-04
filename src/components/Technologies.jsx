260878989eeea68c160a1fb4af78894e05f20dde
Whoistolu <digittolu25@gmail.com>
2024-11-05 00:21:33 +0100

    Re-style the Technologies component

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -13,28 +13,28 @@ const Technologies = () => {
         <div className='border-b border-neutral-500 pb-24'>
             <h1 className="my-20 text-center text-4xl">Technologies</h1>
             <div className="flex flex-wrap  items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
-                    <RiReactjsLine className='text-7xl text-cyan-500' />
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
+                    <RiReactjsLine className='text-7xl text-cyan-800' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                     <DiRuby className='text-7xl text-red-500' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                     <SiRubyonrails className='text-7xl text-red-500' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                     <IoLogoJavascript className='text-7xl text-yellow-500' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                 <RiTailwindCssFill className='text-7xl text-cyan-500'  />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                 <BiLogoPostgresql  className='text-7xl text-blue-400' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                 <FaBootstrap  className='text-7xl text-purple-600' />
                 </div>
-                <div className="rounded-2xl border-4 border-neutral-500 p-4 ">
+                <div className="rounded-2xl border-4 border-neutral-800 p-4 ">
                 <FaCss3Alt  className='text-7xl text-blue-600' />
                 </div>
                 
