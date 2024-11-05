e8dc1f52a1e20a2b50a88bb9850074eb27a0818a
Whoistolu <digittolu25@gmail.com>
2024-11-05 23:15:19 +0100

    Add  styles to the Experince component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { EXPERIENCES } from '../constants/index';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className='border-b border-neutral-300 pb-4'>
+      <h1 className='my-20 text-center text-4xl'>
+        Experience
+      </h1>
+      <div>
+        {EXPERIENCES.map((experince, index) => (
+            <div key={index} className='mb-8 flex flex-wrap lg;justify-center '>
+                <div className='w-full lg:1/4 '>
+                    {experince.year}
+                </div>
+            </div>
+        ))}
+      </div>
+        </div>
   )
 }
 
