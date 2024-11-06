eaf6802ab5c529c6f6f994f9de6d044a89b1c3c8
Whoistolu <digittolu25@gmail.com>
2024-11-06 12:32:56 +0100

    Import and render Contact component in the App file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/About'
 import Technologies from './components/Technologies'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Technologies />
       <Experience />
       <Projects />
+      <Contact />
     </div>
     </div>
   )
