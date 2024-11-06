650854dd56999db7ea15f16955e085fd37257e5d
Whoistolu <digittolu25@gmail.com>
2024-11-06 16:27:25 +0100

    Add some transitions

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -9,10 +9,14 @@ const Legend = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Toluwase Ajise</h1>
-            <span className='bg-gradient-to-r from-orange-400 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-4xl text-transparent'>
+            <motion.h1
+            initial={{x: -100, opacity: 0}}
+            animate={{x:0, opacity: 1}}
+            transition={{duration: 0.5, delay:  0.5}}
+            className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Toluwase Ajise</motion.h1>
+            <motion.span className='bg-gradient-to-r from-orange-400 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-4xl text-transparent'>
               Full Stack Dveloper
-            </span>
+            </motion.span>
             <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{LEGEND_CONTENT}</p>
           </div>
         </div>
