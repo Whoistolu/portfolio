b0bf189f0212cec29ecaf5d891550c28f5331705
Whoistolu <digittolu25@gmail.com>
2024-11-06 16:55:41 +0100

    Complete the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { CONTACT } from '../constants';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <div className='border-b border-neutral-500 pb-20'>
+            <h1 className='my-10 text-center text-4xl'>Get in Touch</h1>
+            <div className="text-center tracking-tighter">
+                <p className='my-4'>{CONTACT.address}</p>
+                <a href="#" className='border-b'>{CONTACT.email}</a>
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
