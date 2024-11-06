dd09154323f4af1ad23e52e79182aa5371d16431
Whoistolu <digittolu25@gmail.com>
2024-11-06 17:02:21 +0100

    Add transition to the About component image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,15 @@ const About = () => {
                 <span className="text-neutral-400 ml-2">Me</span>
             </h1>
             <div className='flex flex-wrap'>
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <motion.div
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x: -100 }}
+                transition={{duration: 0.5}}
+                 className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
                         <img className='rounded-2xl w-10/12' src={aboutimage} alt='about-p' />
                     </div>
-                </div>
+                </motion.div>
                 <div className='w-full lg:w-1/2 '>
                     <div className='flex justify-center lg:justify-start' >
                         <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
