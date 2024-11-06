762d917f8c25e54453ee9825ef3821f8b929616f
Whoistolu <digittolu25@gmail.com>
2024-11-06 09:11:14 +0100

    Re-styled the experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,13 +8,26 @@ const Experience = () => {
         Experience
       </h1>
       <div>
-        {EXPERIENCES.map((experince, index) => (
-            <div key={index} className='mb-8 flex flex-wrap lg;justify-center '>
-                <div className='w-full lg:1/4 '>
-                    {experince.year}
+        {EXPERIENCES.map((experience, index) => (
+            <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+                <div className='w-full lg:1/4'>
+                   <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
+                </div>
+                <div className='w-full max-w-xl lg:w-3/4 mb-2'>
+                    <h6 className='mb-2 font-semibold'>{experience.role} - {" "}
+                        <span className='text-sm text-purple-100'>
+                            {experience.company}
+                        </span>
+                    </h6>
+                    <p className='mb-4 text-neutral-400'>{experience.description}</p>
+                    {experience.technologies.map((tech, index) => (
+                        <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 txt-sm font-medium text-purple-800 '>
+                            {tech}
+                        </span>
+                    ))}
                 </div>
             </div>
-        ))}
+        ))} 
       </div>
         </div>
   )
