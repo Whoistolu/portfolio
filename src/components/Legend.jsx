d2be36c9c7fe58d94c080466e5aee8cafb2063ed
Whoistolu <digittolu25@gmail.com>
2024-11-06 16:54:57 +0100

    Animate image in the Legend component

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -39,7 +39,12 @@ const Legend = () => {
         </div>
         <div className='lg:w-1/2 lg:p-8'>
           <div className="flex justify-center">
-            <img src={profilepicture} alt='profile-p' />
+            <motion.img
+            initial={{x: 100, opacity: 0}}
+            animate={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 1.2}}
+            src={profilepicture}
+            alt='profile-p' />
           </div>
         </div>
       </div>
