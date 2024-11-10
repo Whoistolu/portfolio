9de3b4f1cad6afffbb50fd9bad5f4159f9d34d5e
Whoistolu <digittolu25@gmail.com>
2024-11-10 10:06:39 +0100

    Add project image

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LEGEND_CONTENT } from '../constants';
 import profilepicture from '../assets/profilep.png'
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const delayContainer = (delay) => ({
   hidden: {x: -100, opacity: 0},
